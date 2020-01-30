2207f891afbee7e0c58426f41e61cd95daa816c1
Yong Jian Ming <jianming1993@gmail.com>
2020-01-30 22:14:39 +0800

    windows scripts completed

--- scripts/win_wrapper_build.bat
+++ scripts/win_wrapper_build.bat
@@ -1,13 +1,7 @@
 @echo off
 
-SETLOCAL 
+dotnet build dotnetOQS\dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard1.2 -c Debug -o bin\Debug\dotnetOQS-netstandard1.2
+dotnet build dotnetOQS\dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard2.0 -c Debug -o \bin\Debug\dotnetOQS-netstandard2.0
 
-SET platform=AnyCPU
-
-dotnet build dotnetOQS\dotnetOQS.csproj /p:Platform=%platform% -f netstandard1.2 -c Debug -o bin\Debug\dotnetOQS-wrapper-netstandard1.2
-dotnet build dotnetOQS\dotnetOQS.csproj /p:Platform=%platform% -f netstandard2.0 -c Debug -o \bin\Debug\dotnetOQS-wrapper-netstandard2.0
-
-dotnet build dotnetOQS\dotnetOQS.csproj /p:Platform=%platform% -f netstandard1.2 -c Release -o bin\Release\dotnetOQS-wrapper-netstandard1.2
-dotnet build dotnetOQS\dotnetOQS.csproj /p:Platform=%platform% -f netstandard2.0 -c Release -o bin\Release\dotnetOQS-wrapper-netstandard2.0
-
-ENDLOCAL
+dotnet build dotnetOQS\dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard1.2 -c Release -o bin\Release\dotnetOQS-netstandard1.2
+dotnet build dotnetOQS\dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard2.0 -c Release -o bin\Release\dotnetOQS-netstandard2.0
